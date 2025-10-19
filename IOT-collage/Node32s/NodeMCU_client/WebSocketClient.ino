52340789206b60d12d11ed5342afb258d6d72ba2
Saher Hassaballa <saherayman290@gmail.com>
2025-10-19 19:03:34 +0300

    node cliet connection

---
+++ IOT-collage/Node32s/NodeMCU_client/WebSocketClient.ino
@@ -0,0 +1,142 @@
+/*
+ * WebSocketClient.ino
+ *
+ *  Created on: 24.05.2015
+ *
+ */
+
+#include <Arduino.h>
+
+#include <WiFi.h>
+#include <WiFiMulti.h>
+#include <WiFiClientSecure.h>
+
+#include <WebSocketsClient.h>
+
+WiFiMulti WiFiMulti;
+WebSocketsClient webSocket;
+
+#define USE_SERIAL Serial
+
+void hexdump(const void *mem, uint32_t len, uint8_t cols = 16)
+{
+	const uint8_t *src = (const uint8_t *)mem;
+	USE_SERIAL.printf("\n[HEXDUMP] Address: 0x%08X len: 0x%X (%d)", (ptrdiff_t)src, len, len);
+	for (uint32_t i = 0; i < len; i++)
+	{
+		if (i % cols == 0)
+		{
+			USE_SERIAL.printf("\n[0x%08X] 0x%08X: ", (ptrdiff_t)src, i);
+		}
+		USE_SERIAL.printf("%02X ", *src);
+		src++;
+	}
+	USE_SERIAL.printf("\n");
+}
+
+void webSocketEvent(WStype_t type, uint8_t *payload, size_t length)
+{
+
+	switch (type)
+	{
+	case WStype_DISCONNECTED:
+		USE_SERIAL.printf("[WSc] Disconnected!\n");
+		break;
+	case WStype_CONNECTED:
+		USE_SERIAL.printf("[WSc] Connected to url: %s\n", payload);
+
+		// send message to server when Connected
+		webSocket.sendTXT("Connected");
+		break;
+	case WStype_TEXT:
+	{
+		USE_SERIAL.printf("[WSc] get text: %s\n", payload);
+
+		if (strcmp((char *)payload, "on") == 0)
+		{
+			int value = 255;
+			analogWrite(LED_BUILTIN, value);
+			USE_SERIAL.printf("Received ON -> value: %d\n", value);
+		}
+		else if (strcmp((char *)payload, "off") == 0)
+		{
+			int value = 0;
+			analogWrite(LED_BUILTIN, value);
+			USE_SERIAL.printf("Received OFF -> value: %d\n", value);
+		}
+		else
+		{
+
+			if (value < 0)
+				value = 0;
+			if (value > 255)
+				value = 255;
+			analogWrite(LED_BUILTIN, value);
+			USE_SERIAL.printf("Received VALUE -> value: %d\n", value);
+		}
+		break;
+	}
+
+	// send message to server
+	// webSocket.sendTXT("message here");
+	break;
+	case WStype_BIN:
+		USE_SERIAL.printf("[WSc] get binary length: %u\n", length);
+		hexdump(payload, length);
+
+		// send data to server
+		// webSocket.sendBIN(payload, length);
+		break;
+	case WStype_ERROR:
+	case WStype_FRAGMENT_TEXT_START:
+	case WStype_FRAGMENT_BIN_START:
+	case WStype_FRAGMENT:
+	case WStype_FRAGMENT_FIN:
+		break;
+	}
+}
+
+void setup()
+{
+	// USE_SERIAL.begin(921600);
+	USE_SERIAL.begin(115200);
+
+	// Serial.setDebugOutput(true);
+	USE_SERIAL.setDebugOutput(true);
+
+	USE_SERIAL.println();
+	USE_SERIAL.println();
+	USE_SERIAL.println();
+
+	for (uint8_t t = 4; t > 0; t--)
+	{
+		USE_SERIAL.printf("[SETUP] BOOT WAIT %d...\n", t);
+		USE_SERIAL.flush();
+		delay(1000);
+	}
+
+	WiFiMulti.addAP("Omar", "omar510omar");
+
+	// WiFi.disconnect();
+	while (WiFiMulti.run() != WL_CONNECTED)
+	{
+		delay(100);
+	}
+
+	// server address, port and URL
+	webSocket.begin("192.168.1.37", 3000, "/");
+
+	// event handler
+	webSocket.onEvent(webSocketEvent);
+
+	// use HTTP Basic Authorization this is optional remove if not needed
+	webSocket.setAuthorization("user", "Password");
+
+	// try ever 5000 again if connection has failed
+	webSocket.setReconnectInterval(5000);
+}
+
+void loop()
+{
+	webSocket.loop();
+}
